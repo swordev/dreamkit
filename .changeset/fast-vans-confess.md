af0637554c6579dbf31667a8e47807242ece5baf
Juanra GM <juanrgm724@gmail.com>
2024-11-13 01:42:48 +0100

    fix(site): add import

---
+++ .changeset/fast-vans-confess.md
@@ -0,0 +1,5 @@
+---
+"@dreamkit/site": patch
+---
+
+Fix playground
--- packages/site/src/pages/entry/_examples/home-route.tsx
+++ packages/site/src/pages/entry/_examples/home-route.tsx
@@ -1,3 +1,5 @@
+import { $route } from "dreamkit";
+
 export const homeRoute = $route.path("/").create(() => {
   return <>hello world</>;
 });
