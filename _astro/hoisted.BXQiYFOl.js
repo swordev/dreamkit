93859053ddc51cd71f489c1c853f1742fd28b555
juanrgm <54169354+juanrgm@users.noreply.github.com>
2024-11-15 02:25:31 +0000

    Deploying to gh-pages-next from @ swordev/dreamkit@36cd460008b1ff84898baac60e1a595945e2ec15 🚀

--- _astro/hoisted.BXQiYFOl.js
+++
@@ -1,6 +0,0 @@
-import{o}from"./stackblitz.DMSkREiM.js";import"./hoisted.bnIRwXly.js";const r=`import { $route } from "dreamkit";
-
-export const homeRoute = $route.path("/").create(() => {
-  return <>hello world</>;
-});
-`,t=document.querySelector('a[href="#playground"]');t.addEventListener("click",e=>{e.preventDefault(),o({appCode:r})});
---
+++ _astro/hoisted.BeEVZJ2G.js
@@ -0,0 +1,6 @@
+import{o}from"./stackblitz.csRlJibp.js";import"./hoisted.bnIRwXly.js";const r=`import { $route } from "dreamkit";
+
+export default $route.path("/").create(() => {
+  return <>Hello World</>;
+});
+`,t=document.querySelector('a[href="#playground"]');t.addEventListener("click",e=>{e.preventDefault(),o({appCode:r})});
