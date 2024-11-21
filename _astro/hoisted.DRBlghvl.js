762ae4fa71f1a57a2c6c68bf42a99fed48ecc7f6
juanrgm <54169354+juanrgm@users.noreply.github.com>
2024-11-21 12:25:00 +0000

    Deploying to gh-pages-next from @ swordev/dreamkit@4a8366cee318f630f5e0449ac090766ed144dafc 🚀

--- _astro/hoisted.DRBlghvl.js
+++ _astro/hoisted.DRBlghvl.js
@@ -1,4 +1,4 @@
-import{o}from"./stackblitz.U47L8xJu.js";import"./hoisted.bnIRwXly.js";const r=`import { $route } from "dreamkit";
+import{o}from"./stackblitz.DiVSC2Fk.js";import"./hoisted.bnIRwXly.js";const r=`import { $route } from "dreamkit";
 
 export default $route.path("/").create(() => {
   return <>Hello World</>;
