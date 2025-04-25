39ebdf6d7a807d79c21bd6cb233bae460f3509a0
Juanra GM <juanrgm724@gmail.com>
2025-04-25 16:49:29 +0200

    chore: appy workspace changes

--- workspace.mjs
+++ workspace.mjs
@@ -1,86 +1,118 @@
 // @ts-check
-import { defineConfig } from "@dreamkit/workspace";
+import {
+  defineConfig,
+  getRootTSConfigReferences,
+  getTSConfigReferences,
+} from "@dreamkit/workspace";
 import { readFileSync } from "fs";
 import { markdownTable } from "markdown-table";
 
-const readmeTpl = readFileSync("./docs/readme.tpl.md", "utf8");
 const readmePackageTpl = readFileSync("./docs/readme-package.tpl.md", "utf8");
 
-export default defineConfig((pkg, { packages, dir }) => {
-  if (pkg.name === "@dreamkit/root") {
-    const badgeName = (name) => name.replace("@", "").replace("/", "_");
-    const dreamkitPkg = packages.find(
-      (pkg) => pkg.manifest.name === "dreamkit",
-    );
-    if (!dreamkitPkg) throw new Error("dreamkit package not found");
-
-    const mainPackages = [dreamkitPkg];
-    const internalPackages = packages.filter(
-      (pkg) =>
-        !pkg.manifest.private &&
-        pkg.manifest.name !== "@dreamkit/site" &&
-        pkg !== dreamkitPkg,
-    );
-
-    /**
-     *
-     * @param {import("@dreamkit/workspace").Package[]} pkgs
-     * @returns
-     */
-    const renderTable = (pkgs) =>
-      markdownTable([
-        ["Name", "Version", "Description"],
-        ...pkgs.map((pkg, index) => [
-          `[${pkg.manifest.name}](${pkg.dir})`,
-          `[![npm-badge-${badgeName(pkg.manifest.name)}]](https://www.npmjs.com/package/${pkg.manifest.name})`,
-          pkg.manifest.description,
-        ]),
-      ]);
-    const packagesBadges = [...mainPackages, ...internalPackages]
-      .map(
-        (pkg) =>
-          `[npm-badge-${badgeName(pkg.manifest.name)}]: https://img.shields.io/npm/v/${pkg.manifest.name}`,
-      )
-      .join("\n");
-    return {
-      files: {
-        "README.md": readmeTpl
-          .replaceAll("{packagesTable}", renderTable(mainPackages))
-          .replaceAll("{internalPackagesTable}", renderTable(internalPackages))
-          .replaceAll("{packagesBadges}", packagesBadges),
-      },
-    };
-  }
-
-  if (!pkg.files) throw new Error('"files" field is required');
-
+export default defineConfig(({ pkg, packages }) => {
+  if (!pkg.isRoot && !pkg.manifest.files)
+    throw new Error('"files" field is required');
   if (pkg.name === "@dreamkit/site") return;
-
   return {
     files: {
-      "README.md": readmePackageTpl
-        .replaceAll("{packageName}", pkg.name ?? "")
-        .replaceAll("{packageDescription}", pkg.description ?? ""),
-      "package.json": {
-        ...pkg,
-        type: pkg.name === "@dreamkit/prettier-config" ? "commonjs" : "module",
-        sideEffects: false,
-        author: "Juanra GM",
-        license: "MIT",
-        homepage: "https://dreamkit.dev",
-        repository: {
-          type: "git",
-          url: new URL(`https://github.com/swordev/dreamkit/tree/main/${dir}`)
-            .href,
+      ...(pkg.isRoot && {
+        "tsconfig.build.json": {
+          include: [],
+          references: getRootTSConfigReferences(packages, {
+            exclude: ["@dreamkit/site"],
+          }),
         },
-        contributors: [
-          {
-            name: "Juanra GM",
-            email: "juanrgm724@gmail.com",
-            url: "https://github.com/juanrgm",
+        "README.md": renderRootReadme(packages),
+      }),
+      ...(!pkg.isRoot && {
+        ...(pkg.isTypeScript && {
+          "tsconfig.json": {
+            compilerOptions: { noEmit: true, rootDir: "." },
+            extends: "./tsconfig.build.json",
+            include: ["src", "test"],
           },
-        ],
-      },
+          "tsconfig.build.json": {
+            extends: "@dreamkit/tsconfig/lib.json",
+            references: getTSConfigReferences(pkg, {
+              exclude: ["@dreamkit/tsconfig"],
+            }),
+          },
+        }),
+        "README.md": readmePackageTpl
+          .replaceAll("{packageName}", pkg.name ?? "")
+          .replaceAll("{packageDescription}", pkg.manifest.description ?? ""),
+        "package.json": {
+          ...pkg.manifest,
+          type:
+            pkg.name === "@dreamkit/prettier-config" ? "commonjs" : "module",
+          sideEffects: false,
+          author: "Juanra GM",
+          license: "MIT",
+          homepage: "https://dreamkit.dev",
+          repository: {
+            type: "git",
+            url: new URL(
+              `https://github.com/swordev/dreamkit/tree/main/${pkg.dir}`,
+            ).href,
+          },
+          contributors: [
+            {
+              name: "Juanra GM",
+              email: "juanrgm724@gmail.com",
+              url: "https://github.com/juanrgm",
+            },
+          ],
+        },
+      }),
     },
   };
 });
+
+/**
+ *
+ * @param {import("@dreamkit/workspace").Package[]} packages
+ * @returns
+ */
+export function renderRootReadme(packages) {
+  /**
+   * @param {string} name
+   * @returns {string}
+   */
+  const badgeName = (name) => name.replace("@", "").replace("/", "_");
+  const dreamkitPkg = packages.find((pkg) => pkg.name === "dreamkit");
+  if (!dreamkitPkg) throw new Error("dreamkit package not found");
+
+  const mainPackages = [dreamkitPkg];
+  const internalPackages = packages.filter(
+    (pkg) =>
+      !pkg.manifest.private &&
+      pkg.name !== "@dreamkit/site" &&
+      pkg !== dreamkitPkg,
+  );
+
+  /**
+   *
+   * @param {import("@dreamkit/workspace").Package[]} pkgs
+   * @returns
+   */
+  const renderTable = (pkgs) =>
+    markdownTable([
+      ["Name", "Version", "Description"],
+      ...pkgs.map((pkg, index) => [
+        `[${pkg.name}](${pkg.dir})`,
+        `[![npm-badge-${badgeName(pkg.name)}]](https://www.npmjs.com/package/${pkg.name})`,
+        pkg.manifest.description,
+      ]),
+    ]);
+  const packagesBadges = [...mainPackages, ...internalPackages]
+    .map(
+      (pkg) =>
+        `[npm-badge-${badgeName(pkg.name)}]: https://img.shields.io/npm/v/${pkg.name}`,
+    )
+    .join("\n");
+
+  return readFileSync("./docs/readme.tpl.md", "utf8")
+    .replaceAll("{packagesTable}", renderTable(mainPackages))
+    .replaceAll("{internalPackagesTable}", renderTable(internalPackages))
+    .replaceAll("{packagesBadges}", packagesBadges);
+}
