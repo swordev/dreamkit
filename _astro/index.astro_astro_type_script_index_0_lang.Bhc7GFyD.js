bd760b595070382f627d6d0be92ab5cbd6dee667
juanrgm <54169354+juanrgm@users.noreply.github.com>
2025-09-01 11:33:44 +0000

    Deploying to gh-pages-next from @ swordev/dreamkit@9fc89ad3b9fd166f130a8a7f9afb0eb9fd7298ed 🚀

--- _astro/index.astro_astro_type_script_index_0_lang.Bhc7GFyD.js
+++ _astro/index.astro_astro_type_script_index_0_lang.Bhc7GFyD.js
@@ -1,4 +1,4 @@
-import{o}from"./stackblitz.CR9PErYr.js";const r=`import { $route } from "dreamkit";
+import{o}from"./stackblitz.CtkRkp4d.js";const r=`import { $route } from "dreamkit";
 
 export default $route.path("/").create(() => {
   return <>Hello World</>;
