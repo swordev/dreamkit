32804c4d66aadc9f207e863ab44cf1452600ddc2
Juanra GM <juanrgm724@gmail.com>
2025-10-08 13:10:26 +0200

    refactor(workspace): add type checking

--- packages/workspace/src/utils/object.js
+++ packages/workspace/src/utils/object.js
@@ -1,9 +1,21 @@
+// @ts-check
+/**
+ *
+ * @param {any} input
+ * @returns {Boolean}
+ */
 function isPlainObject(input) {
   return Object.prototype.toString.call(input) === "[object Object]";
 }
 
+/**
+ *
+ * @param {any} object
+ * @returns {any}
+ */
 export function sortObject(object) {
   if (!isPlainObject(object)) return object;
+  /** @type {Record<string, string>} */
   let newObject = {};
   Object.keys(object)
     .sort()
@@ -14,6 +26,11 @@ export function sortObject(object) {
   return newObject;
 }
 
+/**
+ * @param {any} object1
+ * @param {any} object2
+ * @returns {boolean}
+ */
 export function compareObjects(object1, object2) {
   return (
     JSON.stringify(sortObject(object1)) === JSON.stringify(sortObject(object2))
--- packages/workspace/src/utils/pnpm.js
+++ packages/workspace/src/utils/pnpm.js
@@ -1,3 +1,4 @@
+// @ts-check
 import { spawnSync } from "child_process";
 
 /**
