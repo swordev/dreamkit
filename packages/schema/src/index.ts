84c47c3671d1b8b6f91563fd50a27f263c5b9d59
Juanra GM <juanrgm724@gmail.com>
2025-11-01 21:06:44 +0100

    refactor(schema): export type flag

--- packages/schema/src/index.ts
+++ packages/schema/src/index.ts
@@ -1,5 +1,6 @@
 export type { InferType } from "./infer.js";
 export type { ExactType } from "./utils/typescript.js";
+export { TypeFlag } from "./flags.js";
 export * from "./types/index.js";
 export { s } from "./s.js";
 export {
