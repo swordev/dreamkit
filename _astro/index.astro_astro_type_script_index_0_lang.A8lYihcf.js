abb3fbf1126e8c4b8e10f8f1884d91972ef9991d
juanrgm <54169354+juanrgm@users.noreply.github.com>
2025-11-01 20:15:40 +0000

    Deploying to gh-pages-next from @ swordev/dreamkit@49ee603fd68b97807749b3896cf9e1c009e0f271 🚀

--- _astro/index.astro_astro_type_script_index_0_lang.A8lYihcf.js
+++ _astro/index.astro_astro_type_script_index_0_lang.A8lYihcf.js
@@ -1,4 +1,4 @@
-import{o}from"./stackblitz.DUiP0yK6.js";const r=`import { $route } from "dreamkit";
+import{o}from"./stackblitz.ogtKFtVv.js";const r=`import { $route } from "dreamkit";
 
 export default $route.path("/").create(() => {
   return <>Hello World</>;
--- _astro/stackblitz.ogtKFtVv.js
+++ _astro/stackblitz.ogtKFtVv.js
@@ -133,7 +133,7 @@ export default defineConfig({
     "@solidjs/meta": "^0.29.4",
     "@solidjs/router": "^0.15.3",
     "@solidjs/start": "^1.1.3",
-    "dreamkit": "^0.0.37",
+    "dreamkit": "^0.0.38",
     "solid-js": "^1.9.5",
     "vinxi": "^0.5.4"
   },
